fca8e3415a5f71dd7d9f9899fe1f3b8eef74c327
mbprogamer <65442297+mbprogamer@users.noreply.github.com>
2020-05-16 10:31:14 +0200

    init
    init

---
+++ index.js
@@ -0,0 +1,69 @@
+const discord = require("discord.js")
+const botConfig = require("./botconfig.json")
+
+const fs = require("fs");
+
+const bot = new discord.Client();
+
+
+
+
+bot.on("ready", async () => {
+
+    console.log(`${bot.user.username} is online`)
+
+    bot.user.setActivity("mathijsbols.nl", {type: "PLAYING"})
+
+})
+
+bot.on("guildMemberAdd", member => {
+
+    var role = member.guild.roles.find("name", "testbot");
+
+    member.addRole(role);
+});
+
+
+
+
+bot.on("message", async message => {
+
+
+    if (message.author.bot) return;
+
+    if (message.author.type === "dm") return;
+
+    var prefix =botConfig.prefix;
+
+    var messageArray = message.content.split(" ");
+
+    var command = messageArray[0];
+
+    var arguments = messageArray.slice(1);
+
+    if( command === `hallo`){
+        console.log(`hallo`);
+        
+    }
+   
+
+    if( command === `${prefix}info`){
+
+        var botIcon = bot.user.displayAvatarURL;
+        
+        var botEmbed = new discord.RichEmbed()
+            .setDescription("discord bot info")
+            .setColor("#29e53f")
+            .setThumbnail(botIcon)
+            .addField("Bot naam", bot.user.username);
+
+            
+        return message.channel.send(botEmbed);
+    }
+
+        
+
+});
+
+
+bot.login(process.env.token);
